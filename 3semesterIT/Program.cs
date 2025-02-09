359fb54b47c44370922173529c6ee7b0c604a82c
Frank Skorstengaard <1612-au651342@user.noreply.gitlab.au.dk>
2025-02-09 19:43:23 +0100

    never finished 2349, think i need a priority queue

--- 3semesterIT/Program.cs
+++ 3semesterIT/Program.cs
@@ -16,8 +16,14 @@ using ThirdSemesterIT.B1Leetcode;
 //{
 //    Console.WriteLine($"{person.Name}, Age: {person.Age}, PhoneNumber {person.Phone}");
 //}
-for (int i = 1; i < 25; i++)
-{
-    Console.WriteLine($"{i} ++ {Q1510StoneIV.WinnerSquareGame(i)}");
-}
 
+Q2349NumberContainers containers = new Q2349NumberContainers();
+
+Console.WriteLine(containers.Find(10));
+containers.Change(2, 10);
+containers.Change(1, 10);
+containers.Change(3, 10);
+containers.Change(5, 10);
+Console.WriteLine(containers.Find(10));
+containers.Change(1, 20);
+Console.WriteLine(containers.Find(10));
