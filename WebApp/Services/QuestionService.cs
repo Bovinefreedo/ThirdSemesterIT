3cb41486baa3eb570c54d2e339188d3b585e4ac4
Frank Skorstengaard <1612-au651342@user.noreply.gitlab.au.dk>
2025-02-17 07:48:43 +0100

    commin somtein good

---
+++ WebApp/Services/QuestionService.cs
@@ -0,0 +1,28 @@
+﻿using Core.Models;
+
+namespace WebApp.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        private HttpClient _httpClient;
+        private string _baseAPI;
+        public QuestionService(HttpClient httpClient, IConfiguration config) { 
+            _httpClient = httpClient;
+            _baseAPI = config["baseAPI"];
+        }    
+        public Task<Question> getQuestionById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Question>> getQuestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Question> validateQuestion(AOnly answer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
