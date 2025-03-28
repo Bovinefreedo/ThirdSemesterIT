df8d8b301e20e05a1b5fc22d93dacc086bbb3f40
Frank Skorstengaard <1612-au651342@user.noreply.gitlab.au.dk>
2025-03-29 00:15:45 +0100

    Added to maze solver

--- 3semesterIT/B2FunWithLinkedLists/MazeSolver.cs
+++ 3semesterIT/B2FunWithLinkedLists/MazeSolver.cs
@@ -19,12 +19,46 @@ namespace ThirdSemesterIT.B2FunWithLinkedLists
         // "r" : Route to exit
         // s, S and C could be replaced with w simplify the code, but they are useful to
         public string[,] maze { get; set; }
-        public LinkedMazeList linkedList { get; set; } = new();
-        public DoubleLinkedMazeList doubleList { get; set; } = new();
 
         public MazeSolver(string[,] maze)
         {
             this.maze = maze;
+        }
+
+        public int solveMazeBFS(Coordinate start) {
+            DoubleLinkedMazeList que = new DoubleLinkedMazeList();
+            que.addLast(start);
+
+            while (que.first != null) { 
+                maze[que.first.coordinate.x, que.first.coordinate.y] = "c";
+
+                
+                
+
+            }
+
+            return -1;
+        }
+
+        public bool legalMove(Coordinate coordinate)
+        {
+            if (coordinate.x < 0 || coordinate.x >= maze.GetLength(0)) {
+                return false;
+            }
+            if (coordinate.y < 0 || coordinate.y >= maze.GetLength(1))
+            {
+                return false;
+            }
+
+            if (maze[coordinate.x, coordinate.y] == ",")
+            {
+                return true;
+            }
+            else if (maze[coordinate.x, coordinate.y] == "e")
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
